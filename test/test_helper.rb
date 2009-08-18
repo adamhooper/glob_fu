e1b20ec4585b2746134f787c9642050d5d6be9d9
adamh <adam@adamhooper.com>
2009-08-18 14:01:47 -0400

    Initial commit to glob_fu.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'glob_fu'
+
+class Test::Unit::TestCase
+end
