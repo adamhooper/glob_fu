8a007b77bcb3cf73d7a1bbfc6e63d0b284fc95f2
adamh <adam@adamhooper.com>
2009-08-19 11:25:51 -0400

    First code

---
+++ lib/glob_fu.rb
@@ -0,0 +1,65 @@
+module GlobFu
+  class << self
+    def find(*args, &block)
+      options = if Hash === args.last
+        args.pop
+      else
+        {}
+      end
+      results = do_find(args, options)
+
+      if block
+        results.each(&block)
+      end
+      results
+    end
+
+    private
+
+    def do_find(args, options)
+      if [:root, :suffix].any? { |k| options[k] }
+        args = args.dup
+      end
+
+      if options[:root]
+        args.collect! { |a| File.join(options[:root], a) }
+      end
+
+      if options[:suffix]
+        args.collect! { |a| "#{a}.*" }
+      end
+
+      ret = args.inject([]) { |r,a| r.concat(do_single_find(a, options)) }
+      ret.uniq!
+      ret
+    end
+
+    def do_single_find(path, options)
+      ret = Dir[path]
+
+      if options[:suffix]
+        n_dots = File.basename(path).count('.')
+        basename_regex = (['[^\.]+'] * n_dots).join('\.')
+
+        suffix_regex = ""
+        if options[:extra_suffix]
+          suffix_regex << "\.#{Regexp.quote(options[:extra_suffix])}"
+        end
+        suffix_regex << "\.#{Regexp.quote(options[:suffix])}"
+        if options[:optional_suffix]
+          suffix_regex << "(\.#{Regexp.quote(options[:optional_suffix])})?"
+        end
+
+        regex = %r{(/|^)#{basename_regex}#{suffix_regex}$}
+        ret.reject! { |f| !(f =~ regex) }
+        if options[:optional_suffix]
+          ret.reject! { |f| f =~ /\.#{Regexp.quote(options[:suffix])}$/ && File.exist?("#{f}.#{options[:optional_suffix]}") }
+        end
+      end
+
+      ret.sort!
+
+      ret
+    end
+  end
+end
---
+++ spec/glob_fu_spec.rb
@@ -0,0 +1,126 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe(GlobFu) do
+  after(:each) do
+    wipe_fs
+  end
+
+  describe('#find') do
+    it('should find /full/path/to/x') do
+      fs('x')
+      f("#{prefix}/x").should == ["#{prefix}/x"]
+    end
+
+    it('should order x before y') do
+      Dir.should_receive(:[]).and_return(["#{prefix}/y", "#{prefix}/x"])
+      f("#{prefix}/*").should == ["#{prefix}/x", "#{prefix}/y"]
+    end
+
+    it('should order first-glob before second-glob') do
+      fs('y', 'x')
+      f('y', 'x', :root => prefix).should == ["#{prefix}/y", "#{prefix}/x"]
+    end
+
+    it('should find /full/path/to/*') do
+      fs('x')
+      f("#{prefix}/*").should == ["#{prefix}/x"]
+    end
+
+    it('should iterate over results') do
+      fs('x')
+      yielded = nil
+      f("#{prefix}/x") { |f| yielded = f }
+      yielded.should == "#{prefix}/x"
+    end
+
+    it('should glob "**"') do
+      fs('x/y')
+      f("#{prefix}/**/y").should == ["#{prefix}/x/y"]
+    end
+
+    it('should use :root') do
+      fs('x')
+      f('x', :root => prefix).should == ["#{prefix}/x"]
+    end
+
+    it('should allow two arguments') do
+      fs('x', 'y')
+      f('x', 'y', :root => prefix).should == ["#{prefix}/x", "#{prefix}/y"]
+    end
+
+    it('should remove duplicates') do
+      fs('x')
+      f('x', 'x', :root => prefix).should == ["#{prefix}/x"]
+    end
+
+    it('should use :suffix') do
+      fs('x.txt')
+      f('x', :root => prefix, :suffix => 'txt').should == ["#{prefix}/x.txt"]
+    end
+
+    it('should ignore double-suffixed when using :suffix') do
+      fs('x.X.txt')
+      f('*', :root => prefix, :suffix => 'txt').should == []
+    end
+
+    it('should use :optional_suffix to find files') do
+      fs('x.txt.gz')
+      f('x', :root => prefix, :suffix => 'txt', :optional_suffix => 'gz').should == ["#{prefix}/x.txt.gz"]
+    end
+
+    it('should prefer :optional_suffix over other') do
+      fs('x.txt', 'x.txt.gz')
+      f('x', :root => prefix, :suffix => 'txt', :optional_suffix => 'gz').should == ["#{prefix}/x.txt.gz"]
+    end
+
+    it('should fall back to non-:optional_suffix if it is not there') do
+      fs('x.txt')
+      f('x', :root => prefix, :suffix => 'txt', :optional_suffix => 'gz').should == ["#{prefix}/x.txt"]
+    end
+
+    it('should allow :extra_suffix') do
+      fs('x.X.txt')
+      f('x', :root => prefix, :suffix => 'txt', :extra_suffix => 'X').should == ["#{prefix}/x.X.txt"]
+    end
+
+    it('should ignore un-:extra_suffixed') do
+      fs('x.txt')
+      f('x', :root => prefix, :suffix => 'txt', :extra_suffix => 'X').should == []
+    end
+
+    it('should allow :extra_suffix and :optional_suffix in combination') do
+      fs('x.X.txt.gz')
+      f('x', :root => prefix, :suffix => 'txt', :extra_suffix => 'X', :optional_suffix => 'gz').should == ["#{prefix}/x.X.txt.gz"]
+    end
+  end
+
+  private
+
+  def f(*args, &block)
+    GlobFu.find(*args, &block)
+  end
+
+  def prefix
+    @prefix ||= File.dirname(__FILE__) + '/deleteme'
+  end
+
+  def fs(*files)
+    wipe_fs
+    FileUtils.mkdir(prefix)
+
+    files.each do |file|
+      path = File.join(prefix, file)
+      dir = File.dirname(path)
+      unless File.exist?(dir)
+        FileUtils.mkdir_p(dir)
+      end
+      File.open(path, 'w') { |f| f.write("stub\n") }
+    end
+  end
+
+  def wipe_fs
+    if File.exist?(prefix)
+      FileUtils.rm_r(prefix)
+    end
+  end
+end
